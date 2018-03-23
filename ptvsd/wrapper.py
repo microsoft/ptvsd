793f1755ac8cbefa77302e6c582d10fd5738f665
Don Jayamanne <don.jayamanne@yahoo.com>
2018-03-23 15:42:13 -0700

    remove colon

--- ptvsd/wrapper.py
+++ ptvsd/wrapper.py
@@ -784,7 +784,7 @@ class VSCodeMessageProcessor(ipcjson.SocketIO, ipcjson.IpcChannel):
         if self.launch_arguments.get('fixFilePathCase', False):
             self.path_casing.enable()
 
-        options = self.launch_arguments.get('options', ''):
+        options = self.launch_arguments.get('options', '')
         redirect_output = 'STDOUT\tSTDERR' if 'REDIRECT_OUTPUT=True' in options else ''
         self.pydevd_request(pydevd_comm.CMD_REDIRECT_OUTPUT, redirect_output)
 
