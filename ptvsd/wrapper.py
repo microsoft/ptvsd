02b51ac5a3c115f25bfeb0ef579219b8d12b6277
Don Jayamanne <don.jayamanne@yahoo.com>
2018-04-14 01:14:25 -0700

    linter issues

--- ptvsd/wrapper.py
+++ ptvsd/wrapper.py
@@ -31,7 +31,7 @@ pydevd_constants.MAXIMUM_VARIABLE_REPRESENTATION_SIZE = 2**32
 
 import _pydevd_bundle.pydevd_comm as pydevd_comm  # noqa
 import _pydevd_bundle.pydevd_extension_api as pydevd_extapi  # noqa
-import _pydevd_bundle.pydevd_extension_utils as pydevd_extutil # noqa
+import _pydevd_bundle.pydevd_extension_utils as pydevd_extutil  # noqa
 #from _pydevd_bundle.pydevd_comm import pydevd_log
 
 import ptvsd.ipcjson as ipcjson  # noqa
@@ -863,7 +863,9 @@ class VSCodeMessageProcessor(ipcjson.SocketIO, ipcjson.IpcChannel):
         self.pydevd_request(pydevd_comm.CMD_RUN, '')
 
         if self.start_reason == 'attach':
-            # Send event notifying the creation of the process
+            # Send event notifying the creation of the process.
+            # If we do not do this and try to pause, VSC throws errors,
+            # complaining about debugger still initializing.
             with self.is_process_created_lock:
                 if not self.is_process_created:
                     self.is_process_created = True
@@ -1095,6 +1097,7 @@ class VSCodeMessageProcessor(ipcjson.SocketIO, ipcjson.IpcChannel):
                     return 0
 
         server_filename = pydevd_file_utils.norm_file_to_server(filename)
+
         # If the mapped file is the same as the file we provided,
         # then we can generate a soure reference.
         if server_filename == filename and os.path.exists(server_filename):
