09c83542b2f1a735987c13d7f0feffca41e66b2e
Don Jayamanne <don.jayamanne@yahoo.com>
2018-04-15 19:54:18 -0700

    continue execution even with breakpoints

--- ptvsd/pydevd/_pydevd_bundle/pydevd_process_net_command.py
+++ ptvsd/pydevd/_pydevd_bundle/pydevd_process_net_command.py
@@ -249,11 +249,18 @@ def process_net_command(py_db, cmd_id, seq, text):
                 # command to add some breakpoint.
                 # text is file\tline. Add to breakpoints dictionary
                 suspend_policy = "NONE"
+                continue_execution = None
                 hit_condition = None
                 if py_db._set_breakpoints_with_id:
                     try:
-                        breakpoint_id, type, file, line, func_name, condition, expression, hit_condition = text.split('\t', 7)
-                    except:
+                        breakpoint_id, type, file, line, func_name, condition, expression, hit_condition, continue_execution = text.split('\t', 8)
+                        if continue_execution == 'True':
+                            continue_execution = True
+                        elif continue_execution == 'False':
+                            continue_execution = False
+                        else:
+                            continue_execution = None
+                    except Exception:
                         breakpoint_id, type, file, line, func_name, condition, expression = text.split('\t', 6)
 
                     breakpoint_id = int(breakpoint_id)
@@ -298,7 +305,7 @@ def process_net_command(py_db, cmd_id, seq, text):
                     expression = None
 
                 if type == 'python-line':
-                    breakpoint = LineBreakpoint(line, condition, func_name, expression, suspend_policy, hit_condition=hit_condition)
+                    breakpoint = LineBreakpoint(line, condition, func_name, expression, suspend_policy, hit_condition=hit_condition, continue_execution=continue_execution)
                     breakpoints = py_db.breakpoints
                     file_to_id_to_breakpoint = py_db.file_to_id_to_line_breakpoint
                     supported_type = True
@@ -306,7 +313,7 @@ def process_net_command(py_db, cmd_id, seq, text):
                     result = None
                     plugin = py_db.get_plugin_lazy_init()
                     if plugin is not None:
-                        result = plugin.add_breakpoint('add_line_breakpoint', py_db, type, file, line, condition, expression, func_name, hit_condition=hit_condition)
+                        result = plugin.add_breakpoint('add_line_breakpoint', py_db, type, file, line, condition, expression, func_name, hit_condition=hit_condition, continue_execution=continue_execution)
                     if result is not None:
                         supported_type = True
                         breakpoint, breakpoints = result
