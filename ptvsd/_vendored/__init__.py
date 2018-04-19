6b400b19de24ce999f4d7583e2548f7aa8fc6880
Eric Snow <ericsnowcurrently@gmail.com>
2018-04-19 18:08:53 +0000

    Add ptvsd._vendored.list_all().

--- ptvsd/_vendored/__init__.py
+++ ptvsd/_vendored/__init__.py
@@ -12,6 +12,17 @@ if 'pydevd' not in os.listdir(VENDORED_ROOT):
     VENDORED_ROOT = os.path.dirname(os.path.dirname(__file__))
 
 
+def list_all(resolve=False):
+    """Return the list of vendored projects."""
+    # TODO: Derive from os.listdir(VENDORED_ROOT)?
+    projects = [
+        'pydevd',
+    ]
+    if not resolve:
+        return projects
+    return [project_root(name) for name in projects]
+
+
 def project_root(project):
     """Return the path the root dir of the vendored project."""
     return os.path.join(VENDORED_ROOT, project)
