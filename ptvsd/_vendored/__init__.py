bec8cd6e745642274078b59d1ef0bbb510c0e807
Eric Snow <ericsnowcurrently@gmail.com>
2018-04-19 23:45:52 +0000

    Fix setup.py and tests/__main__.py.

--- ptvsd/_vendored/__init__.py
+++ ptvsd/_vendored/__init__.py
@@ -4,12 +4,14 @@ import os
 import os.path
 import sys
 
+from . import _util
 
-VENDORED_ROOT = os.path.dirname(__file__)
+
+VENDORED_ROOT = os.path.dirname(os.path.abspath(__file__))
 # TODO: Move the "pydevd" git submodule to the ptvsd/_vendored directory
 # and then drop the following fallback.
 if 'pydevd' not in os.listdir(VENDORED_ROOT):
-    VENDORED_ROOT = os.path.dirname(os.path.dirname(__file__))
+    VENDORED_ROOT = os.path.dirname(VENDORED_ROOT)
 
 
 def list_all(resolve=False):
@@ -24,8 +26,52 @@ def list_all(resolve=False):
 
 
 def project_root(project):
-    """Return the path the root dir of the vendored project."""
+    """Return the path the root dir of the vendored project.
+
+    If "project" is an empty string then the path prefix for vendored
+    projects (e.g. "ptvsd/_vendored/") will be returned.
+    """
+    if not project:
+        project = ''
     return os.path.join(VENDORED_ROOT, project)
+
+
+def iter_project_files(project, relative=False, **kwargs):
+    """Yield (dirname, basename, filename) for all files in the project."""
+    if relative:
+        with _util.cwd(VENDORED_ROOT):
+            for result in _util.iter_all_files(project, **kwargs):
+                yield result
+    else:
+        root = project_root(project)
+        for result in _util.iter_all_files(root, **kwargs):
+            yield result
+
+
+def iter_packaging_files(project):
+    """Yield the filenames for all files in the project.
+
+    The filenames are relative to "ptvsd/_vendored".  This is most
+    useful for the "package data" in a setup.py.
+    """
+    # TODO: Use default filters?  __pycache__ and .pyc?
+    prune_dir = None
+    exclude_file = None
+    try:
+        mod = import_module('._{}_packaging'.format(project), __name__)
+    except ImportError:
+        pass
+    else:
+        prune_dir = getattr(mod, 'prune_dir', prune_dir)
+        exclude_file = getattr(mod, 'exclude_file', exclude_file)
+    results = iter_project_files(
+        project,
+        relative=True,
+        prune_dir=prune_dir,
+        exclude_file=exclude_file,
+    )
+    for _, _, filename in results:
+        yield filename
 
 
 def prefix_matcher(*prefixes):
