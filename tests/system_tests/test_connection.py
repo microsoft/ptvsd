2153e645a8bd02554283c26d60dd6e0724c200cb
Eric Snow <ericsnowcurrently@gmail.com>
2018-05-03 18:57:01 +0000

    Increase the timeout.

--- tests/system_tests/test_connection.py
+++ tests/system_tests/test_connection.py
@@ -1,4 +1,8 @@
+from __future__ import print_function
+
+import contextlib
 import time
+import sys
 import unittest
 
 from ptvsd.socket import create_client
@@ -6,7 +10,33 @@ from tests.helpers.proc import Proc
 from tests.helpers.workspace import Workspace
 
 
+@contextlib.contextmanager
+def _retrier(timeout=1, persec=10, verbose=False):
+    steps = int(timeout * persec) + 1
+    delay = 1.0 / persec
+
+    def attempts():
+        for attempt in range(1, steps + 1):
+            if verbose:
+                print('*', end='')
+                sys.stdout.flush()
+            yield attempt
+            if verbose:
+                if attempt % persec == 0:
+                    print()
+                elif (attempt * 2) % persec == 0:
+                    print(' ', end='')
+            time.sleep(delay)
+        else:
+            raise RuntimeError('timed out')
+    yield attempts()
+    print()
+
+
 class RawConnectionTests(unittest.TestCase):
+
+    VERBOSE = False
+    #VERBOSE = True
 
     def setUp(self):
         super(RawConnectionTests, self).setUp()
@@ -30,16 +60,15 @@ class RawConnectionTests(unittest.TestCase):
             '--port', '5678',
             '--file', filename,
         ])
-        proc.VERBOSE = True
+        proc.VERBOSE = self.VERBOSE
         with proc:
-            for _ in range(10):
-                try:
-                    connect(addr)
-                    break
-                except Exception:
-                    time.sleep(0.1)
-            else:
-                raise RuntimeError('timed out')
+            with _retrier(timeout=3, verbose=self.VERBOSE) as attempts:
+                for _ in attempts:
+                    try:
+                        connect(addr)
+                        break
+                    except Exception:
+                        pass
             # Give ptvsd long enough to try sending something.
             connect(addr, wait=0.2)
             # We should be able to handle more connections.
