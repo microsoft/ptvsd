5cd5c549d0f4d5beb19d804772aada1301886b1c
Derek Keeler <dekeeler@microsoft.com>
2018-05-08 15:35:25 -0700

    Update tests of the tests module - add test for setting junit report file - add missing return param for junit file to  all tests - add trivial assert to use junit file return value

--- tests/test_tests___main__.py
+++ tests/test_tests___main__.py
@@ -10,7 +10,7 @@ from .__main__ import convert_argv
 class ConvertArgsTests(unittest.TestCase):
 
     def test_no_args(self):
-        argv, env, runtests, lint = convert_argv([])
+        argv, env, runtests, lint, junit_xml_file = convert_argv([])
 
         self.assertEqual(argv, [
             sys.executable + ' -m unittest',
@@ -23,9 +23,10 @@ class ConvertArgsTests(unittest.TestCase):
         })
         self.assertTrue(runtests)
         self.assertFalse(lint)
+        self.assertNotEqual(junit_xml_file, None)
 
     def test_discovery_full(self):
-        argv, env, runtests, lint = convert_argv([
+        argv, env, runtests, lint, junit_xml_file = convert_argv([
             '-v', '--failfast', '--full',
         ])
 
@@ -41,9 +42,10 @@ class ConvertArgsTests(unittest.TestCase):
         })
         self.assertTrue(runtests)
         self.assertFalse(lint)
+        self.assertNotEqual(junit_xml_file, None)
 
     def test_discovery_quick(self):
-        argv, env, runtests, lint = convert_argv([
+        argv, env, runtests, lint, junit_xml_file = convert_argv([
             '-v', '--failfast', '--quick',
         ])
 
@@ -59,9 +61,10 @@ class ConvertArgsTests(unittest.TestCase):
         })
         self.assertTrue(runtests)
         self.assertFalse(lint)
+        self.assertNotEqual(junit_xml_file, None)
 
     def test_modules(self):
-        argv, env, runtests, lint = convert_argv([
+        argv, env, runtests, lint, junit_xml_file = convert_argv([
             '-v', '--failfast',
             'w',
             'x/y.py:Spam.test_spam'.replace('/', os.sep),
@@ -80,9 +83,12 @@ class ConvertArgsTests(unittest.TestCase):
         })
         self.assertTrue(runtests)
         self.assertFalse(lint)
+        self.assertNotEqual(junit_xml_file, None)
 
     def test_no_network(self):
-        argv, env, runtests, lint = convert_argv(['--no-network'])
+        argv, env, runtests, lint, junit_xml_file = convert_argv([
+            '--no-network'
+            ])
 
         self.assertEqual(argv, [
             sys.executable + ' -m unittest',
@@ -93,9 +99,14 @@ class ConvertArgsTests(unittest.TestCase):
         self.assertEqual(env, {})
         self.assertTrue(runtests)
         self.assertFalse(lint)
+        self.assertNotEqual(junit_xml_file, None)
 
     def test_lint(self):
-        argv, env, runtests, lint = convert_argv(['-v', '--quick', '--lint'])
+        argv, env, runtests, lint, junit_xml_file = convert_argv([
+            '-v',
+            '--quick',
+            '--lint'
+            ])
 
         self.assertEqual(argv, [
             sys.executable + ' -m unittest',
@@ -109,9 +120,10 @@ class ConvertArgsTests(unittest.TestCase):
         })
         self.assertTrue(runtests)
         self.assertTrue(lint)
+        self.assertNotEqual(junit_xml_file, None)
 
     def test_lint_only(self):
-        argv, env, runtests, lint = convert_argv([
+        argv, env, runtests, lint, junit_xml_file = convert_argv([
             '--quick', '--lint-only', '-v',
         ])
 
@@ -119,9 +131,12 @@ class ConvertArgsTests(unittest.TestCase):
         self.assertIsNone(env)
         self.assertFalse(runtests)
         self.assertTrue(lint)
+        self.assertNotEqual(junit_xml_file, None)
 
     def test_coverage(self):
-        argv, env, runtests, lint = convert_argv(['--coverage'])
+        argv, env, runtests, lint, junit_xml_file = convert_argv([
+            '--coverage'
+            ])
 
         self.assertEqual(argv, [
             sys.executable + ' -m unittest',
@@ -134,3 +149,22 @@ class ConvertArgsTests(unittest.TestCase):
         })
         self.assertEqual(runtests, 'coverage')
         self.assertFalse(lint)
+        self.assertNotEqual(junit_xml_file, None)
+
+    def test_specify_junit_file(self):
+        argv, env, runtests, lint, junit_xml_file = convert_argv([
+            '--junit-xml=./my-test-file'
+            ])
+
+        self.assertEqual(argv, [
+            sys.executable + ' -m unittest',
+            'discover',
+            '--top-level-directory', PROJECT_ROOT,
+            '--start-directory', PROJECT_ROOT,
+            ])
+        self.assertEqual(env, {
+            'HAS_NETWORK': '1',
+        })
+        self.assertTrue(runtests)
+        self.assertFalse(lint)
+        self.assertEqual(junit_xml_file, './my-test-file')
