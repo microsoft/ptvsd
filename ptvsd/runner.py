8e8a2bd39d1e75fe4856306581140b868374102b
Eric Snow <ericsnowcurrently@gmail.com>
2018-06-07 21:04:58 +0000

    Factor out VSCLifecycleMsgProcessor.

--- ptvsd/runner.py
+++ ptvsd/runner.py
@@ -7,19 +7,18 @@ import os
 import platform
 import pydevd
 import signal
-import socket
 import sys
 import time
 import threading
 import traceback
-import warnings
 
-from ptvsd import ipcjson, __version__
 from ptvsd.daemon import DaemonClosedError
 from ptvsd.pydevd_hooks import start_client
 from ptvsd.socket import close_socket
-from ptvsd.wrapper import WAIT_FOR_DISCONNECT_REQUEST_TIMEOUT, WAIT_FOR_THREAD_FINISH_TIMEOUT, INITIALIZE_RESPONSE # noqa
+from ptvsd.wrapper import (
+    WAIT_FOR_THREAD_FINISH_TIMEOUT, VSCLifecycleMsgProcessor)
 from pydevd import init_stdout_redirect, init_stderr_redirect
+
 
 HOSTNAME = 'localhost'
 WAIT_FOR_LAUNCH_REQUEST_TIMEOUT = 10000
@@ -147,9 +146,9 @@ class Daemon(object):
 
         self._adapter = VSCodeMessageProcessor(
             client,
-            self._notify_launch,
-            self._handle_vsc_disconnect,
-            self._handle_vsc_close,
+            notify_disconnecting=self._handle_vsc_disconnect,
+            notify_closing=self._handle_vsc_close,
+            notify_launch=self._notify_launch,
         )
         self._adapter.start()
         if self.addhandlers:
@@ -197,7 +196,7 @@ class Daemon(object):
             self._adapter.close()
         close_socket(self._client)
 
-    # internal methods for VSCodeMessageProcessor
+    # internal methods for VSCLifecycleProcessor
 
     def _handle_vsc_disconnect(self, kill=False):
         if not self._closed:
@@ -214,133 +213,13 @@ class Daemon(object):
         self._adapter.send_event('output', category=category, output=output)
 
 
-class VSCodeMessageProcessor(ipcjson.SocketIO, ipcjson.IpcChannel):
+class VSCodeMessageProcessor(VSCLifecycleMsgProcessor):
     """IPC JSON message processor for VSC debugger protocol.
 
     This translates between the VSC debugger protocol and the pydevd
     protocol.
     """
 
-    def __init__(
-            self,
-            socket,
-            notify_launch=lambda: None,
-            notify_disconnecting=lambda: None,
-            notify_closing=lambda: None,
-            logfile=None,
-    ):
-        super(VSCodeMessageProcessor, self).__init__(
-            socket=socket, own_socket=False, logfile=logfile)
-        self._socket = socket
-        self._notify_launch = notify_launch
-        self._notify_disconnecting = notify_disconnecting
-        self._notify_closing = notify_closing
-
-        self.server_thread = None
-        self._closed = False
-
-        # adapter state
-        self.disconnect_request = None
-        self.disconnect_request_event = threading.Event()
-        self._exited = False
-
-    def start(self):
-        # VSC msg processing loop
-        self.server_thread = threading.Thread(
-            target=self.process_messages,
-            name='ptvsd.Client',
-        )
-        self.server_thread.pydev_do_not_trace = True
-        self.server_thread.is_pydev_daemon_thread = True
-        self.server_thread.daemon = True
-        self.server_thread.start()
-
-        # special initialization
-        self.send_event(
-            'output',
-            category='telemetry',
-            output='ptvsd',
-            data={
-                'version': __version__,
-                'nodebug': True
-            },
-        )
-
-    # closing the adapter
-
-    def close(self):
-        """Stop the message processor and release its resources."""
-        if self._closed:
-            return
-        self._closed = True
-
-        self._notify_closing()
-        # Close the editor-side socket.
-        self._stop_vsc_message_loop()
-
-    def _stop_vsc_message_loop(self):
-        self.set_exit()
-        if self._socket:
-            try:
-                self._socket.shutdown(socket.SHUT_RDWR)
-                self._socket.close()
-            except Exception:
-                pass
-
-    def _wait_for_server_thread(self):
-        if self.server_thread is None:
-            return
-        if not self.server_thread.is_alive():
-            return
-        self.server_thread.join(WAIT_FOR_THREAD_FINISH_TIMEOUT)
-
-    def handle_stopped(self, exitcode):
-        """Finalize the protocol connection."""
-        if self._exited:
-            return
-        self._exited = True
-
-        # Notify the editor that the "debuggee" (e.g. script, app) exited.
-        self.send_event('exited', exitCode=exitcode)
-
-        # Notify the editor that the debugger has stopped.
-        self.send_event('terminated')
-
-        # The editor will send a "disconnect" request at this point.
-        self._wait_for_disconnect()
-
-    def _wait_for_disconnect(self, timeout=None):
-        if timeout is None:
-            timeout = WAIT_FOR_DISCONNECT_REQUEST_TIMEOUT
-
-        if not self.disconnect_request_event.wait(timeout):
-            warnings.warn('timed out waiting for disconnect request')
-        if self.disconnect_request is not None:
-            self.send_response(self.disconnect_request)
-            self.disconnect_request = None
-
-    def _handle_disconnect(self, request):
-        self.disconnect_request = request
-        self.disconnect_request_event.set()
-        self._notify_disconnecting(not self._closed)
-        if not self._closed:
-            self.close()
-
-    # VSC protocol handlers
-
-    def on_initialize(self, request, args):
-        self.send_response(request, **INITIALIZE_RESPONSE)
-        self.send_event('initialized')
-
-    def on_configurationDone(self, request, args):
-        self.send_response(request)
-
-    def on_launch(self, request, args):
-        self._notify_launch()
-        self.send_response(request)
-
-    def on_disconnect(self, request, args):
-        self._handle_disconnect(request)
-
     def on_invalid_request(self, request, args):
+        # TODO: docstring
         self.send_response(request, success=True)
