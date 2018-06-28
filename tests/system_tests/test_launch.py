750b38cfb1deec40ea2694a0a727c88f9425ff70
Don Jayamanne <don.jayamanne@yahoo.com>
2018-06-28 12:30:34 -0700

    Added tests for files and modules

--- tests/system_tests/test_launch.py
+++ tests/system_tests/test_launch.py
@@ -19,32 +19,47 @@ from . import (_get_version, _strip_pydevd_output,
 ROOT = os.path.dirname(os.path.dirname(ptvsd.__file__))
 PORT = 9876
 
-class LifecycleTests(LifecycleTestsBase):
+class FileLifecycleTests(LifecycleTestsBase):
+    
+    def create_source_file(self, file_name, source):
+        return self.write_script(file_name, source)
+
+    def get_cwd(self):
+        return None
+
+    def get_test_info(self, source):
+        filepath = self.create_source_file('spam.py', source)
+        env = None
+        expected_module = filepath
+        argv = [filepath]
+        return 'spam.py', filepath, env, expected_module, False, argv, self.get_cwd()
 
     def test_with_output(self):
-        script = dedent("""
+        source = dedent("""
             import sys
             sys.stdout.write('ok')
             sys.stderr.write('ex')
             """)
-        options = {'debugOptions':['RedirectOutput']}
-        filename = self.write_script('spam.py', script)
-        argv = [ filename]
+        options = {'debugOptions': ['RedirectOutput']}
+        filename, filepath, env, expected_module, is_module, argv, cwd = self.get_test_info(source)
+
         with DebugClient(port=PORT) as editor:
             adapter, session = editor.host_local_debugger(
-                argv
+                argv,
+                env=env, 
+                cwd=cwd,
             )
             with session.wait_for_event('exited'):
                 with session.wait_for_event('thread'):
                     (req_initialize, req_launch, req_config, _, _, _
-                     ) = lifecycle_handshake(session, 'launch', options=options)
+                        ) = lifecycle_handshake(session, 'launch', options=options)
 
-                adapter.wait()
-
+            adapter.wait()
+    
         # Skipping the 'thread exited' and 'terminated' messages which
         # may appear randomly in the received list.
         received = list(_strip_newline_output_events(session.received))
-        self.assert_received(received, [
+        self.assert_received(received[:-3], [
             self.new_version_event(session.received),
             self.new_response(req_initialize, **INITIALIZE_RESPONSE),
             self.new_event('initialized'),
@@ -54,28 +69,73 @@ class LifecycleTests(LifecycleTestsBase):
                 'isLocalProcess': True,
                 'systemProcessId': adapter.pid,
                 'startMethod': 'launch',
-                'name': filename,
+                'name': expected_module,
             }),
             self.new_event('thread', reason='started', threadId=1),
             self.new_event('output', category='stdout',  output='ok'),
             self.new_event('output', category='stderr',  output='ex'),
-            self.new_event('thread', reason='exited', threadId=1),
-            self.new_event('exited', exitCode=0),
-            self.new_event('terminated'),
+        ])
+
+
+    def test_with_arguments(self):
+        source = dedent("""
+            import sys
+            print(len(sys.argv))
+            for arg in sys.argv:
+                print(arg)
+            """)
+        options = {'debugOptions': ['RedirectOutput']}
+        filename, filepath, env, expected_module, is_module, argv, cwd = self.get_test_info(source)
+
+        with DebugClient(port=PORT) as editor:
+            adapter, session = editor.host_local_debugger(
+                argv = argv + ['1', 'Hello', 'World'],
+                env=env, 
+                cwd=cwd,
+            )
+            with session.wait_for_event('exited'):
+                with session.wait_for_event('thread'):
+                    (req_initialize, req_launch, req_config, _, _, _
+                        ) = lifecycle_handshake(session, 'launch', options=options)
+
+            adapter.wait()
+    
+        # Skipping the 'thread exited' and 'terminated' messages which
+        # may appear randomly in the received list.
+        received = list(_strip_newline_output_events(session.received))
+        self.assert_received(received[:-3], [
+            self.new_version_event(session.received),
+            self.new_response(req_initialize, **INITIALIZE_RESPONSE),
+            self.new_event('initialized'),
+            self.new_response(req_launch),
+            self.new_response(req_config),
+            self.new_event('process', **{
+                'isLocalProcess': True,
+                'systemProcessId': adapter.pid,
+                'startMethod': 'launch',
+                'name': expected_module,
+            }),
+            self.new_event('thread', reason='started', threadId=1),
+            self.new_event('output', category='stdout',  output='4'),
+            self.new_event('output', category='stdout',  output=expected_module),
+            self.new_event('output', category='stdout',  output='1'),
+            self.new_event('output', category='stdout',  output='Hello'),
+            self.new_event('output', category='stdout',  output='World'),
         ])
 
 
     def test_with_break_points(self):
-        bp_line = 3
-        script = dedent("""
+        source = dedent("""
             a = 1
             b = 2
             c = 3
             """)
-        filename = self.write_script('spam.py', script)
-        argv = [ filename]
+        options = {'debugOptions': ['RedirectOutput']}
+        filename, filepath, env, expected_module, is_module, argv, cwd = self.get_test_info(source)
+
+        bp_line = 4
         breakpoints = [{
-            'source': {'path': filename},
+            'source': {'path': filepath},
             'breakpoints': [
                 {'line': bp_line}
             ],
@@ -83,7 +143,9 @@ class LifecycleTests(LifecycleTestsBase):
 
         with DebugClient(port=PORT, connecttimeout=3.0) as editor:
             adapter, session = editor.host_local_debugger(
-                argv
+                argv,
+                env=env, 
+                cwd=cwd,
             )
             with session.wait_for_event('terminated'):
                 with session.wait_for_event('stopped') as result:
@@ -105,9 +167,14 @@ class LifecycleTests(LifecycleTestsBase):
 
         received = list(_strip_newline_output_events(session.received))
         del received[10]  # Module info for runpy.py
-        del received[10].body.get('stackFrames')[1:3] # Ignore non-user stack trace.
+        stack_frames = received[10].body.get('stackFrames')
+        stack_frames[1]['id'] = 1
+        del stack_frames[1:len(stack_frames)]  # Ignore non-user stack trace.
+        received[10].body['totalFrames'] = 1
 
-        self.assert_received(received, [
+        # Skipping the 'thread exited' and 'terminated' messages which
+        # may appear randomly in the received list.
+        self.assert_received(received[:-3], [
             self.new_version_event(session.received),
             self.new_response(req_initialize, **INITIALIZE_RESPONSE),
             self.new_event('initialized'),
@@ -124,7 +191,7 @@ class LifecycleTests(LifecycleTestsBase):
                 'isLocalProcess': True,
                 'systemProcessId': adapter.pid,
                 'startMethod': 'launch',
-                'name': filename,
+                'name': expected_module,
             }),
             self.new_event('thread', reason='started', threadId=tid),
             self.new_event('stopped', reason='breakpoint', threadId=tid, text=None, description=None),
@@ -133,18 +200,18 @@ class LifecycleTests(LifecycleTestsBase):
                 module={
                     'id': 1,
                     'name': '__main__',
-                    'path': filename,
+                    'path': filepath,
                     'package': None,
                 },
                 reason='new',
             ),
             self.new_response(req_stacktrace, seq=11, **{
-                'totalFrames': 3,
+                'totalFrames': 1,
                 'stackFrames': [{
                     'id': 1,
                     'name': '<module>',
                     'source': {
-                        'path': filename,
+                        'path': filepath,
                         'sourceReference': 0,
                     },
                     'line': bp_line,
@@ -153,12 +220,10 @@ class LifecycleTests(LifecycleTestsBase):
             }),
             self.new_response(req_continue, seq=12),
             self.new_event('continued', seq=13, threadId=tid),
-            self.new_event('thread', seq=14, reason='exited', threadId=tid),
-            self.new_event('exited', seq=15, exitCode=0),
-            self.new_event('terminated', seq=16),
         ])
 
-    @unittest.skip('termination needs fixing')
+
+    @unittest.skip('conditional breakpoints needs fixing')
     def test_with_conditional_break_points(self):
         bp_line = 4
         script = dedent("""
@@ -167,7 +232,7 @@ class LifecycleTests(LifecycleTestsBase):
             for i in range(10):
                 print(i)
             """)
-        filename = self.write_script('spam.py', script)
+        filename = self.create_source_file('spam.py', script)
         argv = [ filename]
         breakpoints = [{
             'source': {'path': filename},
@@ -203,15 +268,13 @@ class LifecycleTests(LifecycleTestsBase):
 
     @unittest.skip('termination needs fixing')
     def test_terminating_program(self):
-        bp_line = 10
         script = dedent("""
             import time
 
             while True:
                 time.sleep(0.1)
             """)
-        filename = self.write_script('spam.py', script)
-        argv = [ filename]
+        filename, filepath, env, expected_module, argv = self.get_test_info(source)
 
         with DebugClient(port=PORT, connecttimeout=3.0) as editor:
             adapter, session = editor.host_local_debugger(
@@ -225,124 +288,30 @@ class LifecycleTests(LifecycleTestsBase):
 
             adapter.wait()
 
-    # def test_launch_ptvsd_client_with_output(self):
-    #     argv = []
-    #     waitscript = dedent("""
-    #         import sys
-    #         sys.stdout.write('ok')
-    #         sys.stderr.write('ex')
-    #         """)
-    #     options = {'debugOptions':['RedirectOutput']}
-    #     filename = self.write_script('spam.py', waitscript)
-    #     script = self.write_debugger_script(filename, 9876, run_as='script')
-    #     with DebugClient(port=9876) as editor:
-    #         adapter, session = editor.host_local_debugger(
-    #             argv,
-    #             script,
 
-    #         )
-    #         with session.wait_for_event('exited'):
-    #             with session.wait_for_event('thread'):
-    #                 (req_initialize, req_launch, req_config, _, _, _
-    #                  ) = lifecycle_handshake(session, 'launch', options=options)
 
-    #             adapter.wait()
+class FileWithCWDLifecycleTests(FileLifecycleTests):
+    def get_cwd(self):
+        return os.path.dirname(__file__)
 
-    #     # Skipping the 'thread exited' and 'terminated' messages which
-    #     # may appear randomly in the received list.
-    #     received = list(_strip_newline_output_events(session.received))
-    #     self.assert_received(received, [
-    #         self.new_version_event(session.received),
-    #         self.new_response(req_initialize, **INITIALIZE_RESPONSE),
-    #         self.new_event('initialized'),
-    #         self.new_response(req_launch),
-    #         self.new_response(req_config),
-    #         self.new_event('process', **{
-    #             'isLocalProcess': True,
-    #             'systemProcessId': adapter.pid,
-    #             'startMethod': 'launch',
-    #             'name': filename,
-    #         }),
-    #         self.new_event('thread', reason='started', threadId=1),
-    #         self.new_event('output', category='stdout',  output='ok'),
-    #         self.new_event('output', category='stderr',  output='ex'),
-    #         self.new_event('thread', reason='exited', threadId=1),
-    #         self.new_event('exited', exitCode=0),
-    #         self.new_event('terminated'),
-    #     ])
 
-    # def test_launch_ptvsd_client_with_break_points(self):
-    #     argv = []
-    #     waitscript = dedent("""
-    #         a = 1
-    #         b = 2
-    #         c = 3
-    #         """)
-    #     options = {'debugOptions':['RedirectOutput']}
-    #     filename = self.write_script('spam.py', waitscript)
-    #     script = self.write_debugger_script(filename, 9876, run_as='script')
-    #     breakpoints = [{
-    #         'source': {'path': filename},
-    #         'breakpoints': [
-    #             {'line': 3}
-    #         ],
-    #     }]
+class ModuleLifecycleTests(FileLifecycleTests):
+    
+    def get_test_info(self, source):
 
-    #     with DebugClient(port=9876, connecttimeout=3.0) as editor:
-    #         adapter, session = editor.host_local_debugger(
-    #             argv,
-    #             script,
+        module_name = 'mymod'
+        filename = '__init__.py'
+        self.workspace.ensure_dir(module_name)
+        self.create_source_file(os.path.join(module_name, '__main__.py'), '')
+        filepath = self.create_source_file(os.path.join(module_name, filename), source)
+        env = {'PYTHONPATH': os.path.dirname(os.path.dirname(filepath))}
+        expected_module = module_name + ':'
+        argv = ['-m', module_name]
 
-    #         )
-    #         with session.wait_for_event('terminated'):
-    #             with session.wait_for_event('stopped') as result:
-    #                 (req_initialize, req_launch, req_config, reqs_bps, _, req_thread
-    #                 ) = lifecycle_handshake(session, 'launch',
-    #                                         breakpoints=breakpoints,
-    #                                         options=options,
-    #                                         threads=True)
+        return  filename, filepath, env, expected_module, True, argv, self.get_cwd()
 
-    #             reqs_bp, = reqs_bps
-    #             tid = result['msg'].body['threadId']
-    #             req_stacktrace2 = session.send_request(
-    #                 'stackTrace',
-    #                 threadId=tid,
-    #             )
 
-    #             with session.wait_for_event('continued'):
-    #                 session.send_request('continue', threadId= 1)
-
-    #             adapter.wait()
-
-    #     # Skipping the 'thread exited' and 'terminated' messages which
-    #     # may appear randomly in the received list.
-    #     received = list(_strip_newline_output_events(session.received))
-    #     self.assert_received(received, [
-    #         self.new_version_event(session.received),
-    #         self.new_response(req_initialize, **INITIALIZE_RESPONSE),
-    #         self.new_event('initialized'),
-    #         self.new_response(req_launch),
-    #         self.new_response(req_thread),
-    #         self.new_response(reqs_bp),
-    #         self.new_response(req_config),
-    #         self.new_event('process', **{
-    #             'isLocalProcess': True,
-    #             'systemProcessId': adapter.pid,
-    #             'startMethod': 'launch',
-    #             'name': filename,
-    #         }),
-    #         self.new_event('thread', reason='started', threadId=tid),
-    #         self.new_event('stopped', reason='breakpoint', threadId=tid),
-    #         self.new_event('module'),
-    #         self.new_event('module'),
-    #         self.new_response(req_stacktrace2, **{
-    #             'totalFrames':2,
-
-    #         }),
-    #         self.new_event('output', category='stdout',  output='bye'),
-    #         self.new_event('thread', reason='exited', threadId=tid),
-    #         self.new_event('exited', exitCode=0),
-    #         self.new_event('terminated'),
-    #     ])
-
+class ModuleWithCWDLifecycleTests(ModuleLifecycleTests, FileWithCWDLifecycleTests):
+    def get_cwd(self):
+        return os.path.dirname(__file__)
 
