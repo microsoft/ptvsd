d90080f3c9ca154a40490a2c144830bd8985462e
Eric Snow <ericsnowcurrently@gmail.com>
2018-07-12 15:14:56 +0000

    Fix test_never_call_wait_for_attach.

--- tests/system_tests/test_enable_attach.py
+++ tests/system_tests/test_enable_attach.py
@@ -30,22 +30,41 @@ class EnableAttachTests(LifecycleTestsBase, unittest.TestCase):
             wait(timeout=3)
             adapter.wait()
 
-    @unittest.skip('fails due to "thread" event never happening')
+    @unittest.skip('fails due to "stopped" event never happening')
     def test_never_call_wait_for_attach(self):
         addr = Address('localhost', PORT)
         filename = self.write_script('spam.py', """
             import sys
+            import threading
+            import time
+
             sys.path.insert(0, {!r})
             import ptvsd
             ptvsd.enable_attach({}, redirect_output=False)
             # <ready>
-            # <wait>
+            print('== ready ==')
+
+            # Allow tracing to be triggered.
+            def wait():
+                # <wait>
+                pass
+            t = threading.Thread(target=wait)
+            t.start()
+            for _ in range(100):  # 10 seconds
+                print('-----')
+                t.join(0.1)
+                if not t.is_alive():
+                    break
+            t.join()
+
+            print('== starting ==')
             # <bp>
+            print('== done ==')
             """.format(PROJECT_ROOT, tuple(addr)),
         )
-        lockfile1 = self.workspace.lockfile()
+        lockfile1 = self.workspace.lockfile('ready.lock')
         _, wait = set_release(filename, lockfile1, 'ready')
-        lockfile2 = self.workspace.lockfile()
+        lockfile2 = self.workspace.lockfile('wait.log')
         done, script = set_lock(filename, lockfile2, 'wait')
 
         bp = find_line(script, 'bp')
@@ -57,22 +76,31 @@ class EnableAttachTests(LifecycleTestsBase, unittest.TestCase):
         }]
 
         #DebugAdapter.VERBOSE = True
+        #DebugClient.SESSION.VERBOSE = True
         adapter = DebugAdapter.start_embedded(addr, filename)
         with adapter:
-            wait(timeout=3)
+            # Wait longer that WAIT_TIMEOUT, so that debugging isn't
+            # immediately enabled in the script's thread.
+            wait(timeout=3.0)
+
             with DebugClient() as editor:
                 session = editor.attach_socket(addr, adapter, timeout=1)
                 with session.wait_for_event('thread') as result:
                     lifecycle_handshake(session, 'attach',
-                                        breakpoints=breakpoints)
+                                        breakpoints=breakpoints,
+                                        threads=True)
                 event = result['msg']
                 tid = event.body['threadId']
-                wait(timeout=1)
+
                 with session.wait_for_event('stopped'):
                     done()
                 session.send_request('continue', threadId=tid)
 
                 adapter.wait()
+        out = str(adapter.output)
+
+        self.assertIn('== ready ==', out)
+        self.assertIn('== starting ==', out)
 
     def test_wait_for_attach(self):
         addr = Address('localhost', PORT)
