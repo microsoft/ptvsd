518c6de9f9d5ac72ce3b8309c6b5bd2bf25cb3ed
Don Jayamanne <don.jayamanne@yahoo.com>
2018-07-16 14:39:06 -0700

    tet

---
+++ tests/system_tests/test_misc.py
@@ -0,0 +1,25 @@
+import os
+import os.path
+
+from tests.helpers.resource import TestResources
+from . import (lifecycle_handshake, LifecycleTestsBase, DebugInfo,
+               _strip_newline_output_events)
+
+TEST_FILES = TestResources.from_module(__name__)
+
+
+class NoOutputTests(LifecycleTestsBase):
+    def run_test_with_no_output(self, debug_info):
+        options = {'debugOptions': ['RedirectOutput']}
+        with self.start_debugging(debug_info) as dbg:
+            session = dbg.session
+            lifecycle_handshake(session, debug_info.starttype, options=options)
+        out = dbg.adapter._proc.output.decode('utf-8')
+
+        if len(out) > 0:
+            self.assertIsNone(out)
+
+    def test_with_no_output(self):
+        filename = TEST_FILES.resolve('nooutput.py')
+        cwd = os.path.dirname(filename)
+        self.run_test_with_no_output(DebugInfo(filename=filename, cwd=cwd))
