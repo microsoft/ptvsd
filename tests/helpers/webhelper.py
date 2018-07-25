271437dd57be0c10dc1e5f5a5336fe6289c366f1
Karthik Nadig <kanadig@microsoft.com>
2018-07-25 13:29:30 -0700

    Switch to using requests

--- tests/helpers/webhelper.py
+++ tests/helpers/webhelper.py
@@ -1,17 +1,12 @@
-try:
-    import urllib.request as url_lib
-except ImportError:
-    import urllib2 as url_lib
+import requests
 
 
 def get_web_string(path, obj):
-    req = url_lib.urlopen(path)
-    data = req.read()
-    if not isinstance(data, str):
-        data = data.decode('utf-8')
+    r = requests.get(path)
+    content = r.text
     if obj is not None:
-        obj['content'] = data
-    return data
+        obj['content'] = content
+    return content
 
 
 def get_web_string_no_error(path, obj):
