888e6e6ae4bb3bf8926e42340d0e59dfaad88c9b
Karthik Nadig <kanadig@microsoft.com>
2018-08-08 12:10:04 -0700

    Move exception into a function.

--- tests/resources/system_tests/test_exceptions/handled_exceptions_attach.py
+++ tests/resources/system_tests/test_exceptions/handled_exceptions_attach.py
@@ -3,8 +3,13 @@ import ptvsd
 ptvsd.enable_attach((sys.argv[1], sys.argv[2]))
 ptvsd.wait_for_attach()
 
-try:
-    raise ArithmeticError('Hello')
-except Exception:
-    pass
+
+def main():
+    try:
+        raise ArithmeticError('Hello')
+    except Exception:
+        pass
+
+
+main()
 sys.stdout.write('end')
--- tests/resources/system_tests/test_exceptions/unhandled_exceptions_attach.py
+++ tests/resources/system_tests/test_exceptions/unhandled_exceptions_attach.py
@@ -3,5 +3,10 @@ import ptvsd
 ptvsd.enable_attach((sys.argv[1], sys.argv[2]))
 ptvsd.wait_for_attach()
 
-raise ArithmeticError('Hello')
+
+def main():
+    raise ArithmeticError('Hello')
+
+
+main()
 sys.stdout.write('end')
