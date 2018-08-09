8af66f7f5cb0b593c54e949031e4c1cba0d7fbd6
Karthik Nadig <kanadig@microsoft.com>
2018-08-09 14:27:21 -0700

    Add re-attach tests

--- tests/system_tests/test_break_into_debugger.py
+++ tests/system_tests/test_break_into_debugger.py
@@ -1,5 +1,7 @@
 import os
 import os.path
+import time
+import unittest
 
 from tests.helpers.debugsession import Awaitable
 from tests.helpers.resource import TestResources
@@ -12,17 +14,19 @@ TEST_FILES = TestResources.from_module(__name__)
 
 
 class BreakIntoDebuggerTests(LifecycleTestsBase):
-    def run_test_attach(self, debug_info):
+    def run_test_attach_or_launch(self, debug_info, end_loop=False):
         options = {'debugOptions': ['RedirectOutput']}
         with self.start_debugging(debug_info) as dbg:
             session = dbg.session
-            stopped = dbg.session.get_awaiter_for_event('stopped')
+            stopped = session.get_awaiter_for_event('stopped')
             (_, req_launch_attach, _, _, _, _) = lifecycle_handshake(
                 session,
                 debug_info.starttype,
                 options=options)
             Awaitable.wait_all(req_launch_attach, stopped)
             thread_id = stopped.event.body['threadId']
+            if end_loop:
+                self.set_var_to_end_loop(session, thread_id)
             session.send_request('continue', threadId=thread_id)
 
         received = list(_strip_newline_output_events(dbg.session.received))
@@ -34,28 +38,61 @@ class BreakIntoDebuggerTests(LifecycleTestsBase):
             self.new_event('terminated'),
         ])
 
-    def run_test_attach2(self, debug_info):
+    def set_var_to_end_loop(self, session, thread_id):
+        # Set count > 1000 to end the loop
+        req_stacktrace = session.send_request(
+            'stackTrace',
+            threadId=thread_id,
+        )
+        req_stacktrace.wait()
+        frames = req_stacktrace.resp.body['stackFrames']
+        frame_id = frames[0]['id']
+        req_scopes = session.send_request(
+            'scopes',
+            frameId=frame_id,
+        )
+        req_scopes.wait()
+        scopes = req_scopes.resp.body['scopes']
+        variables_reference = scopes[0]['variablesReference']
+        req_setvar = session.send_request(
+                'setVariable',
+                variablesReference=variables_reference,
+                name='count',
+                value='1000'
+            )
+        req_setvar.wait()
+
+    def run_test_reattach(self, debug_info):
         options = {'debugOptions': ['RedirectOutput']}
         with self.start_debugging(debug_info) as dbg:
             session = dbg.session
-            stopped = dbg.session.get_awaiter_for_event('stopped')
+            stopped1 = session.get_awaiter_for_event('stopped')
             (_, req_launch_attach, _, _, _, _) = lifecycle_handshake(
                 session,
                 debug_info.starttype,
-                options=options)
-            Awaitable.wait_all(req_launch_attach, stopped)
-            thread_id = stopped.event.body['threadId']
+                options=options,
+                threads=True)
+            Awaitable.wait_all(req_launch_attach, stopped1)
+
+            thread_id = stopped1.event.body['threadId']
             req_disconnect = session.send_request('disconnect', restart=False)
             req_disconnect.wait()
-            dbg.adapter._proc.terminate()
 
-        received = list(_strip_newline_output_events(dbg.session.received))
+            time.sleep(1)
+            (_, req_launch_attach, _, _, _, _) = lifecycle_handshake(
+                session,
+                debug_info.starttype,
+                options=options,
+                threads=True)
+            Awaitable.wait_all(req_launch_attach)
+
+            self.set_var_to_end_loop(session, thread_id)
+            session.send_request('continue', threadId=thread_id)
+
+        received = list(_strip_newline_output_events(session.received))
         self.assert_contains(received, [
-            self.new_event('stopped',
-                           reason='step',
-                           threadId=int(thread_id),
-                           text=None,
-                           description=None),
+            self.new_event('exited', exitCode=0),
+            self.new_event('terminated'),
         ])
 
     def test_attach_enable_wait_and_break(self):
@@ -71,7 +108,7 @@ class BreakIntoDebuggerTests(LifecycleTestsBase):
             starttype='attach',
             attachtype='import',
             )
-        self.run_test_attach(debug_info)
+        self.run_test_attach_or_launch(debug_info)
 
     def test_attach_enable_check_and_break(self):
         # Uses enable_attach followed by a loop that checks if the
@@ -86,7 +123,7 @@ class BreakIntoDebuggerTests(LifecycleTestsBase):
             starttype='attach',
             attachtype='import',
             )
-        self.run_test_attach(debug_info)
+        self.run_test_attach_or_launch(debug_info)
 
     def test_attach_enable_and_break(self):
         # Uses enable_attach followed by break_into_debugger
@@ -100,29 +137,54 @@ class BreakIntoDebuggerTests(LifecycleTestsBase):
             starttype='attach',
             attachtype='import',
             )
-        self.run_test_attach2(debug_info)
+        self.run_test_attach_or_launch(debug_info, end_loop=True)
 
     def test_launch(self):
         filename = TEST_FILES.resolve('launch_test.py')
         cwd = os.path.dirname(filename)
         debug_info = DebugInfo(filename=filename, cwd=cwd)
-        options = {'debugOptions': ['RedirectOutput']}
-        with self.start_debugging(debug_info) as dbg:
-            session = dbg.session
-            stopped = dbg.session.get_awaiter_for_event('stopped')
-            (_, req_launch_attach, _, _, _, _) = lifecycle_handshake(
-                session,
-                debug_info.starttype,
-                options=options)
-            Awaitable.wait_all(req_launch_attach, stopped)
-            thread_id = stopped.event.body['threadId']
-            session.send_request('continue', threadId=thread_id)
+        self.run_test_attach_or_launch(debug_info)
 
-        received = list(_strip_newline_output_events(dbg.session.received))
-        self.assert_contains(received, [
-            self.new_event('output', category='stdout', output='one'),
-            self.new_event('output', category='stdout', output='two'),
-            self.new_event('continued', threadId=thread_id),
-            self.new_event('exited', exitCode=0),
-            self.new_event('terminated'),
-        ])
+    def test_reattach_enable_wait_and_break(self):
+        # Uses enable_attach followed by wait_for_attach
+        # before calling break_into_debugger
+        filename = TEST_FILES.resolve('reattach_test.py')
+        cwd = os.path.dirname(filename)
+        debug_info = DebugInfo(
+            filename=filename,
+            cwd=cwd,
+            argv=['localhost', str(PORT)],
+            env={'PTVSD_WAIT_FOR_ATTACH': 'True'},
+            starttype='attach',
+            attachtype='import',
+            )
+        self.run_test_reattach(debug_info)
+
+    def test_reattach_enable_check_and_break(self):
+        # Uses enable_attach followed by a loop that checks if the
+        # debugger is attached before calling break_into_debugger
+        filename = TEST_FILES.resolve('reattach_test.py')
+        cwd = os.path.dirname(filename)
+        debug_info = DebugInfo(
+            filename=filename,
+            cwd=cwd,
+            argv=['localhost', str(PORT)],
+            env={'PTVSD_IS_ATTACHED': 'True'},
+            starttype='attach',
+            attachtype='import',
+            )
+        self.run_test_reattach(debug_info)
+
+    def test_reattach_enable_and_break(self):
+        # Uses enable_attach followed by break_into_debugger
+        # not is_attached check or wait_for_debugger
+        filename = TEST_FILES.resolve('reattach_test.py')
+        cwd = os.path.dirname(filename)
+        debug_info = DebugInfo(
+            filename=filename,
+            cwd=cwd,
+            argv=['localhost', str(PORT)],
+            starttype='attach',
+            attachtype='import',
+            )
+        self.run_test_reattach(debug_info)
