19b3c5e15b8abb69c48713a00ddde11f29f8ab82
Karthik Nadig <kanadig@microsoft.com>
2018-08-10 12:21:29 -0700

    Fix server attach break into debugger issue

--- ptvsd/wrapper.py
+++ ptvsd/wrapper.py
@@ -43,6 +43,9 @@ from ptvsd.pathutils import PathUnNormcase  # noqa
 from ptvsd.safe_repr import SafeRepr  # noqa
 from ptvsd.version import __version__  # noqa
 from ptvsd.socket import TimeoutError  # noqa
+from ptvsd._remote import (
+    _allow_debug_break as allow_break_into_debugger,
+)
 
 
 WAIT_FOR_THREAD_FINISH_TIMEOUT = 1  # seconds
@@ -50,12 +53,6 @@ WAIT_FOR_THREAD_FINISH_TIMEOUT = 1  # seconds
 
 debug = _util.debug
 debugger_attached = threading.Event()
-
-_debugger_start_reason = None
-
-
-def is_launch():
-    return _debugger_start_reason == 'launch'
 
 #def ipcjson_trace(s):
 #    print(s)
@@ -1100,9 +1097,7 @@ class VSCLifecycleMsgProcessor(VSCodeMessageProcessorBase):
     def on_configurationDone(self, request, args):
         # TODO: docstring
         debugger_attached.set()
-        if self.start_reason == 'launch':
-            global _debugger_start_reason
-            _debugger_start_reason = 'launch'
+        allow_break_into_debugger()
         self.send_response(request)
         self._process_debug_options(self.debug_options)
         self._handle_configurationDone(args)
@@ -1110,9 +1105,7 @@ class VSCLifecycleMsgProcessor(VSCodeMessageProcessorBase):
 
     def on_disconnect(self, request, args):
         debugger_attached.clear()
-        if self.start_reason == 'launch':
-            global _debugger_start_reason
-            _debugger_start_reason = None
+        allow_break_into_debugger(False)
         self._restart_debugger = args.get('restart', False)
 
         # TODO: docstring
