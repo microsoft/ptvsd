2b3538f62118960cda9d9d5752498def8d0da354
fabioz <fabiofz@gmail.com>
2018-08-23 08:42:13 -0300

    On Python 3.7, sys.__breakpointhook__ should not be tested as it'll call the original pdb version.

--- ptvsd/_vendored/pydevd/tests_python/test_debugger.py
+++ ptvsd/_vendored/pydevd/tests_python/test_debugger.py
@@ -1769,7 +1769,15 @@ def test_stop_on_start_regular(case_setup):
         writer.finished_ok = True
 
 
-@pytest.mark.parametrize("filename", ('_debugger_case_breakpoint.py', '_debugger_case_breakpoint2.py'))
+def _get_breakpoint_cases():
+    if sys.version_info >= (3, 7):
+        # Just check breakpoint()
+        return ('_debugger_case_breakpoint.py',)
+    else:
+        # Check breakpoint() and sys.__breakpointhook__ replacement.
+        return ('_debugger_case_breakpoint.py', '_debugger_case_breakpoint2.py')
+
+@pytest.mark.parametrize("filename", _get_breakpoint_cases())
 def test_py_37_breakpoint(case_setup, filename):
     with case_setup.test_file(filename) as writer:
         writer.write_make_initial_run()
