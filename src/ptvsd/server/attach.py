8e7585669bc25583d79c17add0d0074a72765fd7
Karthik Nadig <karthik.nadig@live.com>
2019-08-03 21:36:07 -0700

    Cancel wait for attach if IDE is disconnected

--- src/ptvsd/server/attach.py
+++ src/ptvsd/server/attach.py
@@ -10,8 +10,11 @@ import pydevd
 from ptvsd.common import log, options as common_opts
 from ptvsd.server import multiproc, options as server_opts
 from _pydevd_bundle.pydevd_constants import get_global_debugger
+from _pydevd_bundle.pydevd_utils import CancelWait
 from pydevd_file_utils import get_abs_path_real_path_and_base_from_frame
 
+
+_cancel_wait_for_attach = None
 
 def wait_for_attach(timeout=None):
     """If a remote debugger is attached, returns immediately. Otherwise,
@@ -30,7 +33,9 @@ def wait_for_attach(timeout=None):
         log.info(msg)
         raise AssertionError(msg)
 
-    pydevd._wait_for_attach()
+    global _cancel_wait_for_attach
+    _cancel_wait_for_attach = CancelWait()
+    pydevd._wait_for_attach(cancel=_cancel_wait_for_attach)
 
 
 def enable_attach(
